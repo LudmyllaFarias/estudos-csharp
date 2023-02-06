4b8920afa5e7e38d4332844c1566fcf9590ab6fb
LudmyllaFarias <ludmylla.farias@sysmap.com.br>
2023-02-06 16:49:33 -0300

    Vetor e Matriz

---
+++ curso-csharp/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M, N, i, j;
+
+            Console.Write("Quantas linhas vai ter a matriz:");
+            M = int.Parse(Console.ReadLine());
+            Console.Write("Quantas colunas vai ter a matriz:");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[M, N];
+
+            for(i = 0; i < M; i++)
+            {
+                for(j = 0; j < N; j++)
+                {
+                    Console.Write("Elemento [ " + i + "," + j + "]");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Matriz digitada:");
+
+            for (i = 0; i < M; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    Console.WriteLine(mat[i, j] + ",");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ curso-csharp/vetores/vetores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int N, i;
+
+            Console.Write("Quantos números você vai digitar:");
+            N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            for(i = 0; i < N; i++)
+            {
+                Console.Write("Digite um número: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Números digitados: ");
+            for(i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CI));
+            }
+
+            Console.Read();
+
+        }
+    }
+}
