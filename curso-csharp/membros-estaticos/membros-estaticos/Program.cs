b2679509f4447fd83e1905e27f84ae84dc361b2b
LudmyllaFarias <ludmylla.farias@sysmap.com.br>
2023-02-07 17:39:16 -0300

    Membros estãticos

---
+++ curso-csharp/membros-estaticos/membros-estaticos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace membros_estaticos
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Entre o valor do raio:");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CI));
+
+            Console.WriteLine("Volume: " + vol.ToString("F2", CI));
+
+            Console.WriteLine("PI: " + Calculadora.PI.ToString("F2", CI));
+
+            Console.Read();
+        }
+     
+    }
+}
