ecd9d82ce24d96f5eabc7266446ea6d9b6c1ca1b
LudmyllaFarias <ludmylla.farias@sysmap.com.br>
2023-02-07 17:40:21 -0300

    Orientação a objetos

---
+++ curso-csharp/problema-sem-orientacao-a-objetos/problema-sem-orientacao-a-objetos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace problema_sem_orientacao_a_objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X:");
+            x.A = double.Parse(Console.ReadLine(), CI);
+            x.B = double.Parse(Console.ReadLine(), CI);
+            x.C = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CI);
+            y.B = double.Parse(Console.ReadLine(), CI);
+            y.C = double.Parse(Console.ReadLine(), CI);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine();
+            Console.WriteLine("Area de x =" + areaX.ToString("F4", CI));
+            Console.WriteLine("Area de y =" + areaY.ToString("F4", CI));
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }else
+            {
+                Console.WriteLine("Maior area: Y");
+            }
+
+            Console.Read();
+        }
+        
+    }
+}
